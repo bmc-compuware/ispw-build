e2d235841cb8225b9a8a3a39daf6e8f0d8dc58cb
zhouqr2000 <zhouqr2000@gmail.com>
2021-05-12 15:40:53 -0400

    initial version

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+* ALL BMC SOFTWARE PRODUCTS LISTED WITHIN THE MATERIALS ARE TRADEMARKS OF BMC
+* SOFTWARE, INC. ALL OTHER COMPANY PRODUCT NAMES ARE TRADEMARKS OF THEIR
+* RESPECTIVE OWNERS.
+*
+* (c) Copyright 2021 BMC Software, Inc.
+* This code is licensed under MIT license (see LICENSE.txt for details)
+*/
+const core = require('@actions/core');
+const utils = require('@bmc-compuware/ispw-action-utilities');
