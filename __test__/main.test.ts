d77ddac6b5a56a7edf052e89a1394820bc49fafa
Katie Eluskie <katie_eluskie@bmc.com>
2021-05-26 08:54:49 -0400

    code review changes for build (#1)
    These changes make build more consistent with generate
    https://zsojira.bmc.com/browse/ZENG-173259

--- __test__/main.test.ts
+++ __test__/main.test.ts
@@ -6,16 +6,14 @@
  * This code is licensed under MIT license (see LICENSE.txt for details)
  */
 import {CesRequestBody} from '../src/types/CesRequestBody'
-import {BuildAuto} from '../src/types/BuildAuto'
 import {BuildParms} from '../src/types/BuildParms'
-
+import {Inputs} from '../src/types/Inputs'
 const main = require('../src/main')
 
 describe('entity tests', () => {
-  it('buildparms', async () => {
-    let buildParms: BuildParms = {
+  it('inputs', async () => {
+    let inputs: Inputs = {
       build_automatically: 'build_automatically',
-      level: 'DEV1',
       task_id: '7E53CC8FB3D6, 7E53CC8FB3D6, 7E53CC8FB3D6',
       ces_url: 'http://localhost:48226/',
       ces_token: 'token1',
@@ -24,29 +22,31 @@ describe('entity tests', () => {
       change_type: 'T',
       execution_status: 'E'
     }
-    expect(buildParms.level).toBe('DEV1')
-    expect(buildParms.task_id).toBe('7E53CC8FB3D6, 7E53CC8FB3D6, 7E53CC8FB3D6')
-    expect(buildParms.runtime_configuration).toBe('ISPW')
-    expect(buildParms.build_automatically).toBe('build_automatically')
-    expect(buildParms.ces_token).toBe('token1')
-    expect(buildParms.ces_url).toBe('http://localhost:48226/')
-    expect(buildParms.execution_status).toBe('E')
-    expect(buildParms.change_type).toBe('T')
-    expect(buildParms.srid).toBe('srid1')
+    expect(inputs.task_id).toBe('7E53CC8FB3D6, 7E53CC8FB3D6, 7E53CC8FB3D6')
+    expect(inputs.runtime_configuration).toBe('ISPW')
+    expect(inputs.build_automatically).toBe('build_automatically')
+    expect(inputs.ces_token).toBe('token1')
+    expect(inputs.ces_url).toBe('http://localhost:48226/')
+    expect(inputs.execution_status).toBe('E')
+    expect(inputs.change_type).toBe('T')
+    expect(inputs.srid).toBe('srid1')
   })
 
-  it('buildauto', async () => {
-    let buildAuto: BuildAuto = {
+  it('buildParms', async () => {
+    let buildParms: BuildParms = {
       containerId: 'PLAY004799',
       releaseId: ' ',
       taskLevel: 'DEV2',
       taskIds: ['7E53CC8FB3D6', '7E53CC8FB3D7']
     }
-    expect(buildAuto.containerId).toBe('PLAY004799')
-    expect(buildAuto.releaseId).toBe(' ')
-    expect(buildAuto.taskLevel).toBe('DEV2')
-    expect(buildAuto.taskIds[0]).toBe('7E53CC8FB3D6')
-    expect(buildAuto.taskIds[1]).toBe('7E53CC8FB3D7')
+    expect(buildParms.containerId).toBe('PLAY004799')
+    expect(buildParms.releaseId).toBe(' ')
+    expect(buildParms.taskLevel).toBe('DEV2')
+    expect(buildParms.taskIds).toBeDefined()
+    if (buildParms.taskIds) {
+      expect(buildParms.taskIds[0]).toBe('7E53CC8FB3D6')
+      expect(buildParms.taskIds[1]).toBe('7E53CC8FB3D7')
+    }
   })
 
   it('cesrequestbody', async () => {
@@ -58,5 +58,94 @@ describe('entity tests', () => {
     expect(requestBody.changeType).toBe('S')
     expect(requestBody.execStatus).toBe('I')
     expect(requestBody.runtimeConfiguration).toBe('TPZP')
+  })
+})
+
+describe('main tests', () => {
+  it('getParmsFromInputs', () => {
+    let output = main.getParmsFromInputs(undefined)
+    expect(output).toBeDefined()
+    expect(output).toEqual({})
+    expect(output.taskIds).toBeUndefined()
+
+    output = main.getParmsFromInputs('abc')
+    expect(output).toBeDefined()
+    expect(output).toEqual({taskIds: ['abc']})
+    expect(output.taskIds).toEqual(['abc'])
+
+    output = main.getParmsFromInputs('abc,123')
+    expect(output).toBeDefined()
+    expect(output).toEqual({taskIds: ['abc', '123']})
+    expect(output.taskIds).toEqual(['abc', '123'])
+  })
+
+  it('handleResponseBody', () => {
+    expect(() => {
+      main.handleResponseBody(undefined)
+    }).toThrow('No response was received from the build request.')
+
+    expect(() => {
+      main.handleResponseBody({})
+    }).toThrow('The build request did not complete successfully.')
+
+    expect(() => {
+      main.handleResponseBody({message: 'this is a message'})
+    }).toThrow('this is a message')
+
+    expect(() => {
+      main.handleResponseBody({
+        message: 'this is a message',
+        awaitStatus: {
+          generateFailedCount: 4,
+          statusMsg: 'failure status'
+        }
+      })
+    }).toThrow('There were build failures.')
+
+    expect(
+      main.handleResponseBody({
+        message: 'this is a message',
+        awaitStatus: {
+          generateFailedCount: 0,
+          statusMsg: 'failure status'
+        }
+      })
+    ).toEqual({
+      message: 'this is a message',
+      awaitStatus: {
+        generateFailedCount: 0,
+        statusMsg: 'failure status'
+      }
+    })
+  })
+
+  it('assembleRequestBodyObject', () => {
+    expect(main.assembleRequestBodyObject(undefined, undefined, undefined)).toEqual({})
+
+    expect(main.assembleRequestBodyObject('TPZ', undefined, undefined)).toEqual({
+      runtimeConfiguration: 'TPZ'
+    })
+
+    expect(main.assembleRequestBodyObject(undefined, 'S', undefined)).toEqual({changeType: 'S'})
+
+    expect(main.assembleRequestBodyObject(undefined, undefined, 'H')).toEqual({execStatus: 'H'})
+
+    expect(main.assembleRequestBodyObject('TPZ', 'I', 'H')).toEqual({
+      runtimeConfiguration: 'TPZ',
+      changeType: 'I',
+      execStatus: 'H'
+    })
+  })
+
+  it('getBuildAwaitUrlPath', () => {
+    expect(main.getBuildAwaitUrlPath('ISPW', {})).toEqual('/ispw/ISPW/build-await')
+
+    expect(main.getBuildAwaitUrlPath('ISPW', {taskIds: ['first']})).toEqual(
+      '/ispw/ISPW/build-await?taskId=first'
+    )
+
+    expect(main.getBuildAwaitUrlPath('ISPW', {taskIds: ['first', 'second']})).toEqual(
+      '/ispw/ISPW/build-await?taskId=first&taskId=second'
+    )
   })
 })
