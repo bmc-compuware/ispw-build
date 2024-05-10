14a337e51a42b97e5e08025d0d793181851d9c8f
kkoli <kajal_koli@bmc.com>
2024-05-10 14:32:56 +0530

    updated build to main js

---
+++ dist/ispw-build.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * ALL BMC SOFTWARE PRODUCTS LISTED WITHIN THE MATERIALS ARE TRADEMARKS OF BMC
+ * SOFTWARE, INC. ALL OTHER COMPANY PRODUCT NAMES ARE TRADEMARKS OF THEIR
+ * RESPECTIVE OWNERS.
+ *
+ * (c) Copyright 2021 BMC Software, Inc.
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var main_1 = require("./main");
+main_1.run();
+//# sourceMappingURL=ispw-build.js.map
